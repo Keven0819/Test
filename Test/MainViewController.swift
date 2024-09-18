404efddce149024b8e116cc57de23ff320d59534
Keven <s5210apple@gmail.com>
2024-09-18 21:53:19 +0800

    初版

---
+++ Test/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  Test
+//
+//  Created by imac-2627 on 2024/9/18.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    // MARK: - Property
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - UI Settings
+    
+    // MARK: - IBAction
+    
+    // MARK: - Function
+    
+}
+
+// MARK: - Extensions
